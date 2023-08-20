308a3d607910b030951c197862c8620d5da96cc8
Peter Bloomingdale <PeterBlo@Buffalo.edu>
2023-08-19 23:09:34 -0400

    style header/nav

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <>
+    <div className='App'>
       <Header />
       <Main />
       <Footer />
-    </>
+    </div>
   );
 }
 
